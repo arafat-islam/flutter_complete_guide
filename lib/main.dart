f6173951de83609de442040983afe4a88f48140a
Arafat Islam <60419687+arafat-islam@users.noreply.github.com>
2023-02-01 19:42:40 +0600

    after adding map function

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './question.dart';
+import './answer.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
-    questionIndex++;
-    print(questionIndex);
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
   }
 
+  @override
   Widget build(BuildContext context) {
-    var questions = ["What's your name?", "What's your father's name?"];
+    var questions = [
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      },
+      {
+        'questionText': 'Who\'s your favorite instructor?',
+        'answers': ['Max', 'Max', 'Max', 'Max'],
+      },
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Faltu Application'),
+          title: Text('My First App'),
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
-            ElevatedButton(
-              child: Text('Click Here'),
-              onPressed: answerQuestion,
+            Question(
+              questions[_questionIndex]['questionText'],
             ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
